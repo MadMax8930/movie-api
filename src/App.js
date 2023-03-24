41fe98bcfb0a3700fce14520e48546a51ec67f1e
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-03-24 22:31:43 +0100

    fetch one movie structure

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import searchIcon from './search.svg';
 //3b491425
 const API_URL = 'http://www.omdbapi.com?apikey=3b491425';
 
+const movie1 = {
+  "Title": "Batman v Superman: Dawn of Justice",
+  "Year": "2016",
+  "imdbID": "tt2975590",
+  "Type": "movie",
+  "Poster": "N/A"
+}
+
 const App = () => {
 
   // Asynchronous = takes time to fetch movies
@@ -37,6 +45,21 @@ const App = () => {
       </div>
 
       <div className="container">
+        
+        <div className="movie">   
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+
+          <div>
+            <img src={movie1.Poster !== 'N/A' ? movie1.Poster : "https://via.placeholder.com/400" } alt={movie1.Title}/>
+          </div>
+
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
 
       </div>
     </div>
